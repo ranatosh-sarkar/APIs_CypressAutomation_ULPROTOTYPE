9f1b2aa8de8645224f0dfd94b6f1d29725fded88
Ranatosh-github <62794816+ranatosh-sarkar@users.noreply.github.com>
2025-07-10 02:14:10 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports/html',
+    overwrite: false,
+    html: true,
+    json: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
